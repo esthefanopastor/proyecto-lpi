0350ff0e4641d6bd355625c57de2146d2985596e
Esthefano Oniel Pastor Palacios <esthefanopastordev@gmail.com>
2020-06-21 14:28:42 -0500

    CRUD Marca finalizado

--- src/gui/GUIMarca.java
+++ src/gui/GUIMarca.java
@@ -4,6 +4,9 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -23,7 +26,7 @@ import entity.Marca;
 import model.MarcaModel;
 import util.Validaciones;
 
-public class GUIMarca extends JFrame implements ActionListener {
+public class GUIMarca extends JFrame implements ActionListener, MouseListener {
 
 	/**
 	 * 
@@ -36,6 +39,10 @@ public class GUIMarca extends JFrame implements ActionListener {
 	private JTable table;
 	private JScrollPane scrollPane;
 	private JButton btnAgregar;
+
+	private int selectedID = -1;
+	private JButton btnActualizar;
+	private JButton btnEliminar;
 
 	/**
 	 * Launch the application.
@@ -100,13 +107,15 @@ public class GUIMarca extends JFrame implements ActionListener {
 		btnAgregar.setBounds(480, 67, 165, 33);
 		contentPane.add(btnAgregar);
 
-		JButton btnActualizar = new JButton("ACTUALIZAR");
+		btnActualizar = new JButton("ACTUALIZAR");
+		btnActualizar.addActionListener(this);
 		btnActualizar.setIcon(new ImageIcon(GUIMarca.class.getResource("/iconos/edit.gif")));
 		btnActualizar.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnActualizar.setBounds(480, 112, 165, 33);
 		contentPane.add(btnActualizar);
 
-		JButton btnEliminar = new JButton("ELIMINAR");
+		btnEliminar = new JButton("ELIMINAR");
+		btnEliminar.addActionListener(this);
 		btnEliminar.setIcon(new ImageIcon(GUIMarca.class.getResource("/iconos/delete.gif")));
 		btnEliminar.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnEliminar.setBounds(480, 157, 165, 33);
@@ -117,12 +126,20 @@ public class GUIMarca extends JFrame implements ActionListener {
 		contentPane.add(scrollPane);
 
 		table = new JTable();
+		this.table.addMouseListener(this);
 		table.setModel(new DefaultTableModel(new Object[][] {}, new String[] { "ID", "Nombre", "Estado" }));
 		scrollPane.setViewportView(table);
 
+		listarMarcas();
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == this.btnEliminar) {
+			btnEliminarActionPerformed(e);
+		}
+		if (e.getSource() == this.btnActualizar) {
+			btnActualizarActionPerformed(e);
+		}
 		if (e.getSource() == this.btnAgregar) {
 			btnAgregarActionPerformed(e);
 		}
@@ -145,13 +162,116 @@ public class GUIMarca extends JFrame implements ActionListener {
 			int response = marcaModel.insertMarca(marca);
 
 			if (response > 0) {
+				clearInputs();
+				listarMarcas();
+				selectedID = -1;
 				message("Marca insertada correctamente");
+			} else {
+				message("Error en el registro");
 			}
 		}
 
 	}
 
+	protected void btnActualizarActionPerformed(ActionEvent e) {
+		if (selectedID == -1) {
+			message("Seleccione una fila");
+		} else {
+			String nombre = txtNombre.getText();
+			String estado = txtEstado.getText();
+
+			if (!nombre.matches(Validaciones.NOMBRE_MARCA)) {
+				message("El nombre es de 2 a 45 caracteres");
+			} else if (!estado.matches(Validaciones.ESTADO_MARCA)) {
+				message("El estado es de 2 a 45 caracteres");
+			} else {
+				Marca marca = new Marca();
+				marca.setId(selectedID);
+				marca.setNombre(nombre);
+				marca.setEstado(estado);
+
+				MarcaModel marcaModel = new MarcaModel();
+				int response = marcaModel.actualizarMarca(marca);
+
+				if (response > 0) {
+					clearInputs();
+					listarMarcas();
+					selectedID = -1;
+					message("Se actualizó correctamente la marca");
+				} else {
+					message("Error al actualizar marca");
+				}
+			}
+		}
+	}
+
+	protected void btnEliminarActionPerformed(ActionEvent e) {
+		if (selectedID == -1) {
+			message("Debe seleccionar una fila");
+		} else {
+			MarcaModel marcaModel = new MarcaModel();
+			int response = marcaModel.eliminarMarca(selectedID);
+			if (response > 0) {
+				listarMarcas();
+				clearInputs();
+				selectedID = -1;
+				message("Se eliminó correctamente la marca");
+			} else {
+				message("Error al eliminar");
+			}
+		}
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() == this.table) {
+			tableMouseClicked(e);
+		}
+	}
+
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	public void mouseExited(MouseEvent e) {
+	}
+
+	public void mousePressed(MouseEvent e) {
+	}
+
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	protected void tableMouseClicked(MouseEvent e) {
+		int row = table.getSelectedRow();
+
+		selectedID = (int) table.getValueAt(row, 0);
+		String nombre = (String) table.getValueAt(row, 1);
+		String estado = (String) table.getValueAt(row, 2);
+
+		txtNombre.setText(nombre);
+		txtEstado.setText(estado);
+	}
+
 	void message(String message) {
 		JOptionPane.showMessageDialog(this, message);
 	}
+
+	void clearInputs() {
+		txtNombre.setText("");
+		txtEstado.setText("");
+		txtNombre.requestFocus();
+	}
+
+	void listarMarcas() {
+		DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
+		tableModel.setRowCount(0);
+
+		MarcaModel marcaModel = new MarcaModel();
+		List<Marca> marcas = marcaModel.listarMarcas();
+
+		for (Marca marca : marcas) {
+			Object[] row = { marca.getId(), marca.getNombre(), marca.getEstado() };
+			tableModel.addRow(row);
+		}
+	}
+
 }
--- src/model/MarcaModel.java
+++ src/model/MarcaModel.java
@@ -2,6 +2,10 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import entity.Marca;
 import util.DBConnection;
@@ -36,5 +40,95 @@ public class MarcaModel {
 		}
 
 		return response;
+	}
+
+	public int actualizarMarca(Marca marca) {
+		int response = -1;
+		Connection connection = null;
+		PreparedStatement statement = null;
+		try {
+			connection = DBConnection.getConnection();
+			String query = "UPDATE marca SET nombre=?, estado=? where idmarca=?";
+			statement = connection.prepareStatement(query);
+			statement.setString(1, marca.getNombre());
+			statement.setString(2, marca.getEstado());
+			statement.setInt(3, marca.getId());
+			response = statement.executeUpdate();
+			System.out.println("actualizados :  " + response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (statement != null)
+					statement.close();
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return response;
+	}
+
+	public int eliminarMarca(int idMarca) {
+		int response = -1;
+		Connection connection = null;
+		PreparedStatement statement = null;
+
+		try {
+			connection = DBConnection.getConnection();
+			String query = "DELETE FROM marca WHERE idmarca=?";
+			statement = connection.prepareStatement(query);
+			statement.setInt(1, idMarca);
+			response = statement.executeUpdate();
+			System.out.println("eliminados :  " + response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (statement != null)
+					statement.close();
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return response;
+	}
+
+	public List<Marca> listarMarcas() {
+		ArrayList<Marca> marcas = new ArrayList<Marca>();
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			connection = DBConnection.getConnection();
+			String query = "SELECT *  FROM marca";
+			statement = connection.prepareStatement(query);
+
+			resultSet = statement.executeQuery();
+
+			Marca marca = null;
+			while (resultSet.next()) {
+				marca = new Marca();
+				marca.setId(resultSet.getInt("idmarca"));
+				marca.setNombre(resultSet.getString("nombre"));
+				marca.setEstado(resultSet.getString("estado"));
+				marcas.add(marca);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (statement != null)
+					statement.close();
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return marcas;
 	}
 }
